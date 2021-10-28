d7e0214e87b33815cddf06f18fa2f4c93bf733b4
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2021-10-28 15:14:03 +0300

    Add new internal annotation `IntrinsicConstEvaluation`

---
+++ libraries/stdlib/api/js-v1/kotlin.internal.kt
@@ -0,0 +1,11 @@
+@kotlin.annotation.Target(allowedTargets = {AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY})
+@kotlin.annotation.Retention(value = AnnotationRetention.BINARY)
+public final annotation class IntrinsicConstEvaluation : kotlin.Annotation {
+    public constructor IntrinsicConstEvaluation()
+}
+/*∆*/ 
+/*∆*/ @kotlin.annotation.Target(allowedTargets = {AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY})
+/*∆*/ @kotlin.annotation.Retention(value = AnnotationRetention.BINARY)
+/*∆*/ public final annotation class IntrinsicConstEvaluation : kotlin.Annotation {
+/*∆*/     public constructor IntrinsicConstEvaluation()
+/*∆*/ }
---
+++ libraries/stdlib/api/js/kotlin.internal.kt
@@ -0,0 +1,5 @@
+@kotlin.annotation.Target(allowedTargets = {AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY})
+@kotlin.annotation.Retention(value = AnnotationRetention.BINARY)
+public final annotation class IntrinsicConstEvaluation : kotlin.Annotation {
+    public constructor IntrinsicConstEvaluation()
+}
