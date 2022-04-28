05e75e4755e09daeed40bf931f51fd8f4694d297
Nkolay Krasko <nikolay.krasko@jetbrains.com>
2022-04-28 19:13:55 +0300

    Remove source sets from the instrumentation task

--- buildSrc/src/main/kotlin/InstrumentJava.kt
+++ buildSrc/src/main/kotlin/InstrumentJava.kt
@@ -6,44 +6,56 @@ import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withGroovyBuilder
+import java.io.File
 
 /*
  * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-class InstrumentJava(@Transient val javaInstrumentator: Configuration, @Transient val sourceSet: SourceSet) : Action<Task> {
+class InstrumentJava(@Transient val javaInstrumentator: Configuration) : Action<Task> {
     private val instrumentatorClasspath: String by lazy {
         javaInstrumentator.asPath
     }
 
-    private val srcDirs: FileCollection by lazy {
-        sourceSet.allJava.sourceDirectories
-    }
-
     override fun execute(task: Task) {
         require(task is JavaCompile) { "$task is not of type JavaCompile!" }
-        task.doLast {
-            val anySrcDir = srcDirs.filter { it.exists() }.firstOrNull()
-            if (anySrcDir != null) {
-                task.ant.withGroovyBuilder {
-                    "taskdef"(
-                        "name" to "instrumentIdeaExtensions",
-                        "classpath" to instrumentatorClasspath,
-                        "loaderref" to "java2.loader",
-                        "classname" to "com.intellij.ant.InstrumentIdeaExtensions"
-                    )
-                }
 
-                task.ant.withGroovyBuilder {
-                    "instrumentIdeaExtensions"(
-                        "srcdir" to anySrcDir, // No code should actually be compiled because of areJavaClassesCompiled==true in Javac2 - so any src folder will do.
-                        "destdir" to task.destinationDirectory.asFile.get(),
-                        "classpath" to task.classpath.asPath,
-                        "includeantruntime" to false,
-                        "instrumentNotNull" to true
-                    )
-                }
+        // Should be an existent folder with no java files inside
+        // Check Javac.execute() - https://github.com/apache/ant/blob/9943641/src/main/org/apache/tools/ant/taskdefs/Javac.java#L1086
+        // Also
+        // InstrumentIdeaExtensions - https://github.com/JetBrains/intellij-community/blob/9c40bdd/java/compiler/javac2/src/com/intellij/ant/InstrumentIdeaExtensions.java
+        // Javac2.compile() - https://github.com/JetBrains/intellij-community/blob/9c40bdd/java/compiler/javac2/src/com/intellij/ant/Javac2.java#L237
+        val existentFakeSrcPath = File(task.project.rootProject.projectDir, ".fleet").relativeTo(task.project.projectDir).path.replace("\\", "/")
+
+        task.doLast {
+            task.ant.withGroovyBuilder {
+                "taskdef"(
+                    "name" to "instrumentIdeaExtensions",
+                    "classpath" to instrumentatorClasspath,
+                    "loaderref" to "java2.loader",
+                    "classname" to "com.intellij.ant.InstrumentIdeaExtensions"
+                )
+            }
+
+            val instrumentIdeaExtensionsTask = task.ant.withGroovyBuilder {
+                "instrumentIdeaExtensions"(
+                    "srcdir" to existentFakeSrcPath,
+                    "destdir" to task.destinationDirectory.asFile.get(),
+                    "classpath" to task.classpath.asPath,
+                    "includeantruntime" to false,
+                    "instrumentNotNull" to true
+                )
+            }
+
+            val getFileListMethod = instrumentIdeaExtensionsTask!!.javaClass.getMethod("getFileList")
+            @Suppress("UNCHECKED_CAST")
+            val compileFiles = getFileListMethod.invoke(instrumentIdeaExtensionsTask) as Array<File>
+            if (compileFiles.isNotEmpty()) {
+                error(compileFiles.joinToString(
+                    prefix = "The should be an empty list of files to compile, but now files are:\n",
+                    separator = "   \n"
+                ))
             }
         }
     }
